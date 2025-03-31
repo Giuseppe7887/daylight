549fb8e6403ff6798224b854278b8f86749af135
Giuseppe7887 <giuseppegravante078@gmail.com>
2025-03-31 09:22:22 +0200

    add server types

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -1,7 +1,8 @@
+import type { Request, Response } from "express";
+
 // servers
 import app from "./server";
 
 // functions
-// @ts-ignore
 export const daylight = (req: Request, res: Response) => app(req, res);
   
